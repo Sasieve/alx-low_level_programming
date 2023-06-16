e79af91264c7e0c08cb0579c5b603ac8acc0c9f4
Sasieve <sasieve47@gmail.com>
2023-06-16 06:11:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * c program with a puts function
- * and a 0 return value
+ * and a  return 0 value
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return(0);
 }
