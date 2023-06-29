3bb04aa5aac20ba4c9885f21104ac88b9dc78feb
Sasieve <sasieve47@gmail.com>
2023-06-29 06:04:45 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_strlen-function that returns the length of a string
+ * @s:strlen
+ * Return:(longi)
+ */
+int _strlen(char *s)
+{
+	int longi=0;
+	while(*s != '/0')
+	{
+		longi++;
+		s++;
+        }
+return(longi);
+}
+
