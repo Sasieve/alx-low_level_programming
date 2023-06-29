6b5f812002f8c6305dcc1045a8d9c37d6126c2ac
Sasieve <sasieve47@gmail.com>
2023-06-29 06:23:39 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,11 @@
  */
 int _strlen(char *s)
 {
-	int longi=0;
-
-	while (*s != '/0')
-	{
-		longi++;
-		s++;
-        }
-return (longi);
+       length = 0;
+       
+       	while (*str++)
+				length++;
+	
+		return (length);;
 }
 
