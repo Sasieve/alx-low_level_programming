5fed3c90819a6cef8275fbff87a5dd674491c1a0
Sasieve <sasieve47@gmail.com>
2023-06-30 07:48:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,13 @@
 	int j;
 
 	i=0;
-	while (dest[i] != '\0')
+	while(dest[i] != '\0')
 	{
 		i++;
 
 	}
 	j=0;
-	while (j<n && src[j] != '\0')
+	while(j<n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
