e2f4d19848d049a70effddd4b92c6a10c0558e13
Sasieve <sasieve47@gmail.com>
2023-06-30 07:07:36 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int putchar(char c);
 char *_strcat(char *dest, char *src);
  char *_strncat(char *dest, char *src, int n);
  char *_strncpy(char *dest, char *src, int n);
@@ -13,3 +14,5 @@ char *_strcat(char *dest, char *src);
  void print_number(int n);
  char *infinite_add(char *n1, char *n2, char *r, int size_r);
  void print_buffer(char *b, int size);
+
+#endif
