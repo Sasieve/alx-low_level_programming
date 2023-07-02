ce2ccef4e247c4daec1cda07e0e9c7e73cfdf96b
Sasieve <sasieve47@gmail.com>
2023-07-02 06:59:13 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts2 - prints every other character including first and new line
- * @s:string
+ * @str:string input
  * Return:print
  */
 void puts2(char *str)
@@ -19,7 +19,7 @@ void puts2(char *str)
 	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
-		if (o%2 == 0)
+		if (o % 2 == 0)
 	{
 			_putchar(str[o]);
 		}
